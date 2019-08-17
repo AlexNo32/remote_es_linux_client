f6540af9f1b93fd421468cc240360f14679bf71c
charname <firklaag@gmail.com>
2019-08-17 13:50:35 +1000

    17-8 client request

---
+++ client.h
@@ -0,0 +1,85 @@
+/*
+ ============================================================================
+ Name        : Simple Remote Execution System Linux client
+ Author      : Alex
+ Version     : 0.9v
+ Copyright   : No copyright
+ Description : 2803ICT assignment 1, Ansi-style, CLion + Ubuntu
+ ============================================================================
+ */
+
+#ifndef CLIENT_C_CSOCKET_H
+#define CLIENT_C_CSOCKET_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include "stringbuffer.h"
+
+#ifdef WIN64
+#include <winsock2.h>
+#include <windows.h>
+#include <io.h>
+#pragma comment(lib,"Ws2_32.lib")
+#else
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <sys/select.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/timeb.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+
+typedef unsigned int SOCKET;
+typedef struct sockaddr_in SOCKADDR_IN;
+typedef struct sockaddr SOCKADDR;
+#endif
+
+#define check(A, M, ...) if(!(A)) { perror(M); errno = 0; goto error;}
+
+#define DEFAULTPORT  1024
+#define STDBUF 512
+
+//#define "put" 4, "get" 5, "run" 6, "list" 7, "sys" 8, "quit" 9
+enum PTYPE{
+    PUT = 4, GET, RUN, LIST, SYS, QUIT
+};
+
+SOCKET connector(char *host);
+int make_request(SOCKET sock);
+
+int recv_msg(SOCKET sockFd, Buffer *buf);
+int send_msg(SOCKET sockFd, Buffer *buf);
+
+/* * Package struct
+ *  1. timestamp ()
+ *  2. package type (put, run, sys, etc..)
+ *  3. is force mode? (0/1)
+ *  4. is long list mode? (0/1)
+ *  5. length of arg list ()
+ *  6. arg list ()
+ *  7. dir
+ *  8. length of file list ()
+ *  9. file list ()
+ * */
+typedef struct {
+    unsigned long timeStamp;// length: 13
+    unsigned short ptype; // [4 - 9]
+    unsigned short fmode; // 1: On, 0: Off
+    unsigned short lmode; // 1: On, 0: Off
+    char * dirname; // folder name
+    unsigned short args;
+    unsigned short files;
+    char *argv[10];
+    char *filev[10];
+} Request;
+
+#define LONG_MODE "-l"
+#define FORCE_MODE "-f"
+
+#endif //CLIENT_C_CSOCKET_H
---
+++ csocket.c
@@ -0,0 +1,95 @@
+/*
+ ============================================================================
+ Name        : Simple Remote Execution System Linux client
+ Author      : Alex
+ Version     : 0.9v
+ Copyright   : No copyright
+ Description : 2803ICT assignment 1, Ansi-style, CLion + Ubuntu
+ ============================================================================
+ */
+#include "client.h"
+
+int nonblock(SOCKET fd);
+void closeConnection(SOCKET sock);
+
+/* client connector */
+SOCKET connector(char *host) {
+    /* Initialize Winsock. */
+#ifdef WIN64
+    WSADATA wsaData;
+	check(NO_ERROR == WSAStartup(MAKEWORD(2, 2), &wsaData), "[ERROR] WSAStartup failed. \n");
+#endif
+    SOCKET connector;
+    int sockOpt = 1;
+
+    /* Initial socket address */
+    SOCKADDR_IN addrClient;
+    memset(&addrClient, 0, sizeof(addrClient));
+    addrClient.sin_family = AF_INET;
+    addrClient.sin_addr.s_addr = inet_addr(host);
+    addrClient.sin_port = htons(DEFAULTPORT);
+
+    /* create socket */
+    connector = socket(AF_INET, SOCK_STREAM, 0);
+    check(connector >= 0, "[ERROR] Cannot create a socket... \n");
+    printf("[INFO] Client socket created: %d... \n", connector);
+
+    /* socket setting */
+    setsockopt(connector, SOL_SOCKET, SO_KEEPALIVE, &sockOpt, sizeof(sockOpt));
+    setsockopt(connector, IPPROTO_TCP, TCP_NODELAY, &sockOpt, sizeof(sockOpt));
+    printf("[INFO] Socket setting ... done. \n");
+
+    /* Connect to server */
+    check(connect(connector, (SOCKADDR*)&addrClient, sizeof(addrClient)) == 0, "[ERROR] Cannot connect to server...\n");
+    printf("[INFO] Client socket connect to server: %d... \n", connector);
+
+    /* non-block setting */
+    check(nonblock(connector) == 0, "[ERROR] Unable to set non-blocking mode. \n");
+    printf("[INFO] Non-blocking setting is on: %d... \n", connector);
+
+    return connector;
+
+    error:
+    closeConnection(connector);
+    exit(EXIT_FAILURE);
+}
+
+/* non-block setting */
+int nonblock(SOCKET fd) {
+#ifdef WIN64
+    unsigned long nNonBlocking = 1;
+    return ioctlsocket(fd, FIONBIO, &nNonBlocking);// SOCKET_ERROR :-1
+#else
+    int flags = fcntl(fd, F_GETFL, 0);
+    return fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+#endif
+}
+
+/* clean up socket */
+void closeConnection(SOCKET sock){
+#ifdef WIN64
+    closesocket(sock);
+	WSACleanup(); //Clean up Winsock
+#else
+    close(sock);
+#endif
+}
+
+int recv_msg(SOCKET sockFd, Buffer *buf){
+    int nCount;
+    char buff[STDBUF];
+
+//    while( (nCount = recv(sockFd, buff, STDBUF, MSG_WAITALL)) > 0 ){
+    while( (nCount = recv(sockFd, buff, STDBUF, MSG_DONTWAIT)) > 0 ){
+        buffer_append(buf, buff, nCount);
+    }
+    return 0;
+}
+
+int send_msg(SOCKET sockFd, Buffer *buf){
+    if(send(sockFd, buf->data, buf->size, 0) == -1){
+        perror("[ERROR] send failed ");
+        return -1;
+    }
+    return 0;
+}
